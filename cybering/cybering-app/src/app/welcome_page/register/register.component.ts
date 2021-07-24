0441101398a44ce647b5dca7bfea8166748e1b02
Gregory KalIinikos <grekoskall@gmail.com>
2021-07-24 18:20:34 +0300

    basic ui
    TODO: backend support

--- cybering/cybering-app/src/app/welcome_page/register/register.component.ts
+++ cybering/cybering-app/src/app/welcome_page/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerForm = new FormGroup({
+    emailControl: new FormControl(''),
+    passwordControl: new FormControl('')
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  registerSubmit() {
+    
   }
 
 }
